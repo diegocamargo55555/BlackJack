7a45a79f8335217c2f1e0b0a2c020127a716f187
diegocamargo55555 <137927984+diegocamargo55555@users.noreply.github.com>
2024-05-12 20:37:51 -0300

    envia para uma pilha e função compra

--- baralho.h
+++ baralho.h
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-struct cartas
+struct carta
 {
     string naipe;
     int valor;
@@ -11,15 +11,73 @@ struct cartas
 struct jogador
 {
 	string nome;
-	int mao;
+	int valor_mao;
+	string naipe_mao;
 };
 struct baralho
 {
-    cartas carta;
+    int valor;
+    string nipe;
     baralho *link;
 };
 
+void troca(int *v1, int *v2, string *s1, string *s2  )
+{
+	int temp;
+	string aux;
+	
+	temp = *v1;
+	aux = *s1;
+	
+	*v1 = *v2;
+	*s1 = *s2;
+	
+	*v1 = temp;
+	*s1 = aux;
+}
 
 baralho *inicializaLista(baralho *L) {
 	return NULL;
 };
+
+baralho *PUSH(baralho *L, int v, string x) {
+	baralho *N;
+
+	N = new baralho;
+	N->valor = v;
+	N->nipe = x;
+
+    N->link = L;
+	
+	L = N;
+	return (L);
+}
+
+baralho *pop(baralho *l)
+{
+	baralho *temp;
+	if (l != nullptr)
+	{
+		temp = l;
+		l = l->link;
+		delete temp;
+	}
+	return(l);		
+}
+void puts(baralho* dek) {
+	baralho *P;
+	P = dek;
+
+	while(P != NULL) {
+		printf("carta : %d %s \n", P->valor, (P->nipe).c_str());
+		P = P->link;
+	}	
+}
+
+void compra(baralho *D, int *cart, string *nip)
+{
+	*cart = D->valor;
+	*nip = D->nipe;
+	
+}
+	
--- main.cc
+++ main.cc
@@ -9,59 +9,88 @@ int main()
     srand(time(nullptr));
     
     carta cartas[52];
+    
     baralho *deck;
+    string naipe, aux;
+    int n, k = 0, temp, rand1, rand2;
+    
     inicializaLista(deck);
 
-    string naipe;
-    int n;
-    /*
-    for (int i = 0; i < 52; i++)
-    {
-        int n = rand() % 10 + 1 ;
-        baralho = PUSH(baralho, n);
-        //cout << n << " " ;
-    }*/
     
+    
+    // gera as cartas
     for (int i = 0; i < 4; i++)
     {
         if (i == 0)
         {
-            naipe = "ouros";
+            naipe = " ouros";
         }else if (i == 1)
         {
-            naipe = "paus";
+            naipe = " paus";
         }else if (i ==2)
         {
-            naipe = "espadas";
+            naipe = " espadas";
         }else if (i == 3)
         {
-            naipe = "copas";
+            naipe = " copas";
         }
-        for (int j = 1; j < 14; j++)
+        for (int j = 1; j < 14; j++, k++)
         {
-            if (j > 10)
+            if (j >= 10)
             {
                 n = 10;
-                cartas[i].valor = n;
+                cartas[k].valor = n;
+                cartas[k].naipe = naipe;
             }else{
-                cartas[i].valor = i;
-                cartas[i].naipe = naipe;
+                cartas[k].valor = j;
+                cartas[k].naipe = naipe;
             }
-                
         }
     }
-    /*
-    for (int i = 0; i < 4; i++)
+    // randomiza
+    for (int i = 0; i < 104; i++)
     {
-        for (int j = 1; j < 10; j++)
-        {
-            baralho = PUSH(baralho, j);
-        }
+        rand1 = rand() % 52;
+        rand2 = rand() % 52;
+        
+        //troca(&(cartas[rand1].valor), &(cartas[rand2].valor), &(cartas[rand1].naipe), &(cartas[rand2].naipe));
+        
+        temp = cartas[rand1].valor;
+        aux = cartas[rand1].naipe;
+        
+        cartas[rand1].naipe = cartas[rand2].naipe;
+        cartas[rand2].naipe = aux;
+        
+        cartas[rand1].valor = cartas[rand2].valor;
+        cartas[rand2].valor = temp;
     }
-    */
+    for (int i = 0; i < 52; i++)
+    {
+        deck = PUSH(deck, cartas[i].valor, cartas[i].naipe);
+    }
     
-
+    for (int i = 0; i < 52; i++)
+    {
+        cout << cartas[i].naipe << " " << cartas[i].valor << endl;
+    }
+    
+    int d = 0, n_jogadores;
+    cout << "/--------------------------------------/ " << endl;
+    cin >> n_jogadores;
+    jogador player[2] , dealer;
+    
+    compra(deck, &(player[0].valor_mao), &(player[0].naipe_mao) );
+    deck = pop(deck);
+    
+    compra(deck, &(player[1].valor_mao), &(player[1].naipe_mao) );
+    deck = pop(deck);
+    
+    cout << player[0].valor_mao << player[0].naipe_mao << endl ;
+    cout << player[1].valor_mao << player[1].naipe_mao << endl ;
+    
+    //puts(deck);
     
     
-    puts(baralho);
 }
+
+
